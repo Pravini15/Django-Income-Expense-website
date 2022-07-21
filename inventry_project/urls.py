6f86aca345731b509b4350a4c0b40a0dee2cd635
pravini15 <pravinipw15@gmail.com>
2022-07-21 10:03:17 +0530

    Done

--- inventry_project/urls.py
+++ inventry_project/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path('register/', user_view.register, name='user-registration'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
-
 ]
