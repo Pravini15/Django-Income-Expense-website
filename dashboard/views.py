7cbed7aedeb6eb856e7535f83e4cff4649574779
pravini15 <pravinipw15@gmail.com>
2022-07-22 12:34:25 +0530

    Done

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import ProductForm
 
 @login_required
 # Create your views here.
@@ -14,11 +15,43 @@ def staff(request):
 
 @login_required
 def product(request):
-    items = Product.objects.all()
+    items = Product.objects.all() #using ORM
+    #items = Product.objects.raw('SELECT * FROM dashboard_product')
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm()
+
     context = {
         'items': items,
+        'form': form,
     }
     return render(request, 'dashboard/product.html', context)
+
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method=='POST':
+        item.delete()
+        return redirect('dashboard-product')
+    return render(request, 'dashboard/product_delete.html')
+
+def product_update(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method=='POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm(instance=item)
+    context ={
+        'form': form,
+    }
+    return render(request, 'dashboard/product_update.html', context)
 
 @login_required
 def order(request):
